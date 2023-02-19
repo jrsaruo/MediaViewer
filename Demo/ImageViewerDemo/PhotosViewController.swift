02a9c92357427f8a54050ce025e2167b9a2088d6
Yusaku Nishi <jrsaruo@gmail.com>
2023-02-19 22:26:56 +0900

    add: PhotosVC

--- Demo/ImageViewerDemo/PhotosViewController.swift
+++ Demo/ImageViewerDemo/PhotosViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  PhotosViewController.swift
 //  ImageViewerDemo
 //
 //  Created by Yusaku Nishi on 2023/02/19.
@@ -7,10 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class PhotosViewController: UIViewController {
+    
+    // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Photos"
     }
 }
--- Demo/ImageViewerDemo/SceneDelegate.swift
+++ Demo/ImageViewerDemo/SceneDelegate.swift
@@ -16,8 +16,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = scene as? UIWindowScene else { return }
+        let rootVC = UINavigationController(rootViewController: PhotosViewController())
         window = UIWindow(windowScene: windowScene)
-        window!.rootViewController = ViewController()
+        window!.rootViewController = rootVC
         window!.makeKeyAndVisible()
     }
 }
