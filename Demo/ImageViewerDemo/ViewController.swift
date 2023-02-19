5c736ac72022b2111a8993d8c1cb168572a12386
Yusaku Nishi <jrsaruo@gmail.com>
2023-02-19 22:21:22 +0900

    add: Demo project

---
+++ Demo/ImageViewerDemo/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  ImageViewerDemo
+//
+//  Created by Yusaku Nishi on 2023/02/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
