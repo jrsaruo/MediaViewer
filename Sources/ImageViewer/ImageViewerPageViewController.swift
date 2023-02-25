a331da28288cdbc1b49a17610e92fde1dec4e4c7
Yusaku Nishi <jrsaruo@gmail.com>
2023-02-25 21:21:42 +0900

    add: ImageViewerPageViewController

---
+++ Sources/ImageViewer/ImageViewerPageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ImageViewerPageViewController.swift
+//  
+//
+//  Created by Yusaku Nishi on 2023/02/25.
+//
+
+import UIKit
+
+// TODO: Rename to ImageViewerViewController
+open class ImageViewerPageViewController: UIPageViewController {
+    
+    // MARK: - Initializers
+    
+    public init(image: UIImage) {
+        super.init(transitionStyle: .scroll,
+                   navigationOrientation: .horizontal,
+                   options: [
+                    .interPageSpacing: 16,
+                    .spineLocation: SpineLocation.none.rawValue
+                   ])
+        let imageViewer = ImageViewerViewController(image: image)
+        setViewControllers([imageViewer], direction: .forward, animated: false)
+    }
+    
+    required public init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
