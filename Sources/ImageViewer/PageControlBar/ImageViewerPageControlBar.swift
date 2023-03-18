47ef708aae23cae1646a5e8e592d2d05c40f63a1
Yusaku Nishi <jrsaruo@gmail.com>
2023-03-18 19:44:30 +0900

    add: ImageViewerPageControlBar

---
+++ Sources/ImageViewer/PageControlBar/ImageViewerPageControlBar.swift
@@ -0,0 +1,34 @@
+//
+//  ImageViewerPageControlBar.swift
+//  
+//
+//  Created by Yusaku Nishi on 2023/03/18.
+//
+
+import UIKit
+
+final class ImageViewerPageControlBar: UIView {
+    
+    // MARK: - Initializers
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setUpViews()
+    }
+    
+    private func setUpViews() {
+        
+    }
+    
+    // MARK: - Override
+    
+    override var intrinsicContentSize: CGSize {
+        CGSize(width: super.intrinsicContentSize.width,
+               height: 42)
+    }
+}
