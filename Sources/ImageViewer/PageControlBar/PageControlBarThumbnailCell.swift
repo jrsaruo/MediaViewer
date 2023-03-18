beaec34ea13c1fb27c0b302b9863526c3ca6ccfa
Yusaku Nishi <jrsaruo@gmail.com>
2023-03-19 03:39:45 +0900

    add: PageControlBarThumbnailCell

---
+++ Sources/ImageViewer/PageControlBar/PageControlBarThumbnailCell.swift
@@ -0,0 +1,45 @@
+//
+//  PageControlBarThumbnailCell.swift
+//  
+//
+//  Created by Yusaku Nishi on 2023/03/19.
+//
+
+import UIKit
+
+final class PageControlBarThumbnailCell: UICollectionViewCell {
+    
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    // MARK: - Initializers
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setUpViews()
+    }
+    
+    private func setUpViews() {
+        clipsToBounds = true
+        
+        // Subviews
+        contentView.addSubview(imageView)
+        
+        // Layout
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+    }
+}
