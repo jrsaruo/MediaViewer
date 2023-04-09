e3570c814ae4269dcfecb8e840e8b770bfc0f28a
Yusaku Nishi <jrsaruo@gmail.com>
2023-04-09 22:09:51 +0900

    fix: missing @MainActor

--- Sources/ImageViewer/ImageViewerOnePage/ImageViewerOnePageViewController.swift
+++ Sources/ImageViewer/ImageViewerOnePage/ImageViewerOnePageViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+@MainActor
 protocol ImageViewerOnePageViewControllerDelegate: AnyObject {
     func imageViewerPageTapped(_ imageViewerPage: ImageViewerOnePageViewController)
     
--- Sources/ImageViewer/Transitions/ImageViewerInteractivePopTransition.swift
+++ Sources/ImageViewer/Transitions/ImageViewerInteractivePopTransition.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+@MainActor
 final class ImageViewerInteractivePopTransition: NSObject {
     
     private let sourceImageView: UIImageView?
