6004859fd618daf1170859f475a08155cefd2d5a
Yusaku Nishi <jrsaruo@gmail.com>
2023-10-25 23:15:21 +0900

    add: Logger.ui

---
+++ Sources/ImageViewer/Extensions/Logger+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  Logger+Extension.swift
+//
+//
+//  Created by Yusaku Nishi on 2023/10/25.
+//
+
+import os
+
+extension Logger {
+    static let ui = Logger(subsystem: "jp.co.jrsaruo.ImageViewer", category: "UI")
+}
