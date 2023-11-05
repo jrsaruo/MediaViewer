e8a649662cb021bf82f12a10e23e43ee2b855844
Yusaku Nishi <jrsaruo@gmail.com>
2023-11-05 21:15:29 +0900

    add: MediaViewerDelegate.mediaViewer(_:willBeginPopTransitionInto:)

--- Sources/MediaViewer/MediaViewerDelegate.swift
+++ Sources/MediaViewer/MediaViewerDelegate.swift
@@ -10,6 +10,15 @@ import UIKit
 @MainActor
 public protocol MediaViewerDelegate: AnyObject {
     
+    /// Notifies the delegate before a media viewer is popped from the navigation controller.
+    /// - Parameters:
+    ///   - mediaViewer: A media viewer that will be popped.
+    ///   - sourceView: A view that is the destination of the pop transition for the media viewer.
+    func mediaViewer(
+        _ mediaViewer: MediaViewerViewController,
+        willBeginPopTransitionInto sourceView: UIView?
+    )
+    
     /// Tells the delegate a media viewer has moved to a particular page.
     /// - Parameters:
     ///   - mediaViewer: A media viewer informing the delegate about the page move.
@@ -20,6 +29,12 @@ public protocol MediaViewerDelegate: AnyObject {
 // MARK: - Default implementations -
 
 extension MediaViewerDelegate {
+    
+    public func mediaViewer(
+        _ mediaViewer: MediaViewerViewController,
+        willBeginPopTransitionInto sourceView: UIView?
+    ) {}
+    
     public func mediaViewer(
         _ mediaViewer: MediaViewerViewController,
         didMoveToPage page: Int
