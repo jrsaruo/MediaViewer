390d77e0dca48b54b072aaccd985cab93b59d400
Yusaku Nishi <jrsaruo@gmail.com>
2023-11-26 05:40:04 +0900

    add: CollectionDifference.Change.element

--- Sources/MediaViewer/Extensions/CollectionDifference+Extension.swift
+++ Sources/MediaViewer/Extensions/CollectionDifference+Extension.swift
@@ -30,3 +30,13 @@ extension CollectionDifference {
         return (insertions: insertions, removals: removals)
     }
 }
+
+extension CollectionDifference.Change {
+    
+    var element: ChangeElement {
+        switch self {
+        case .insert(_, let element, _), .remove(_, let element, _):
+            return element
+        }
+    }
+}
