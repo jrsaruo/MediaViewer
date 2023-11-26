3d023feeb312933a3c93b0f1391f910134f5bb4e
Yusaku Nishi <jrsaruo@gmail.com>
2023-11-26 20:47:47 +0900

    docs: fix comment

--- Sources/MediaViewer/Extensions/MediaViewerViewController+UI.swift
+++ Sources/MediaViewer/Extensions/MediaViewerViewController+UI.swift
@@ -11,13 +11,13 @@ extension MediaViewerViewController {
     
     /// Creates and returns a trash button for deleting media on the current page.
     ///
-    /// If you want to provide your custom delete UI, you can build one with `deleteCurrentMedia(after:)` or `deleteMedia(with:after:)` instead.
+    /// If you want to provide your custom delete UI, you can build one with `reloadMedia()`
+    /// method instead.
     ///
     /// - Note: `deleteAction` must complete deletion until it returns.
-    ///         That means the number of media must be reduced by one after the `deleteAction` is succeeded.
-    ///         If the deletion fails, `deleteAction` must throw an error.
+    ///         If the deletion fails, `deleteAction` can throw an error.
     /// - Parameter deleteAction: A closure that takes the current media identifier and
-    ///                           performs the actual media deletion.
+    ///                           performs the media deletion.
     ///                           It must complete deletion until it returns.
     /// - Returns: A trash button for deleting media.
     public func trashButton<MediaIdentifier>(
