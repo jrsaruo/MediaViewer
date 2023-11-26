abfdedafdcf5a1d46abd8848213ba2c91b998984
Yusaku Nishi <jrsaruo@gmail.com>
2023-11-26 15:10:58 +0900

    delete: CollectionDifference.changes

--- Sources/MediaViewer/Extensions/CollectionDifference+Extension.swift
+++ Sources/MediaViewer/Extensions/CollectionDifference+Extension.swift
@@ -5,32 +5,6 @@
 //  Created by Yusaku Nishi on 2023/11/21.
 //
 
-extension CollectionDifference {
-    
-    typealias ChangeAssociatedValues = (
-        offset: Int,
-        element: ChangeElement,
-        associatedWith: Int?
-    )
-    
-    var changes: (
-        insertions: [ChangeAssociatedValues],
-        removals: [ChangeAssociatedValues]
-    ) {
-        var insertions: [ChangeAssociatedValues] = []
-        var removals: [ChangeAssociatedValues] = []
-        for change in self {
-            switch change {
-            case .insert(let offset, let element, let associatedWith):
-                insertions.append((offset, element, associatedWith))
-            case .remove(let offset, let element, let associatedWith):
-                removals.append((offset, element, associatedWith))
-            }
-        }
-        return (insertions: insertions, removals: removals)
-    }
-}
-
 extension CollectionDifference.Change {
     
     var element: ChangeElement {
