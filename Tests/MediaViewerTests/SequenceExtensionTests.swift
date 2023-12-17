cbb20d31575aaa3b38959c3df331cdea2fff6e88
Yusaku Nishi <jrsaruo@gmail.com>
2023-12-17 15:50:55 +0900

    add: testMeasureSubtracting1-3()

--- Tests/MediaViewerTests/SequenceExtensionTests.swift
+++ Tests/MediaViewerTests/SequenceExtensionTests.swift
@@ -18,4 +18,28 @@ final class SequenceExtensionTests: XCTestCase {
         XCTAssertEqual([0, 1, 2].subtracting([]), [0, 1, 2])
         XCTAssertEqual([0, 1, 2].subtracting([100]), [0, 1, 2])
     }
+    
+    func testMeasureSubtracting() {
+        measure {
+            _ = (0..<3000).subtracting(
+                stride(from: 0, to: 3000, by: 1)
+            )
+        }
+    }
+    
+    func testMeasureSubtracting2() {
+        measure {
+            _ = (0..<3000).subtracting2(
+                stride(from: 0, to: 3000, by: 1)
+            )
+        }
+    }
+    
+    func testMeasureSubtracting3() {
+        measure {
+            _ = (0..<3000).subtracting3(
+                stride(from: 0, to: 3000, by: 1)
+            )
+        }
+    }
 }
