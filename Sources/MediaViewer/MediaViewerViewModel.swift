621e2169932d9c0eba1c22b7bd62b2d854003df9
Yusaku Nishi <jrsaruo@gmail.com>
2024-02-14 02:25:20 +0900

    add: MediaViewerVM.pageControlBarInteractivePagingAction(...)

--- Sources/MediaViewer/MediaViewerViewModel.swift
+++ Sources/MediaViewer/MediaViewerViewModel.swift
@@ -104,3 +104,49 @@ extension MediaViewerViewModel {
         return nil
     }
 }
+
+// MARK: - Page control bar interactive paging -
+
+extension MediaViewerViewModel {
+    
+    enum PageControlBarInteractivePagingAction: Hashable {
+        case start(forwards: Bool)
+        case update(progress: Double)
+        case finish
+        case cancel
+    }
+    
+    func pageControlBarInteractivePagingAction(
+        on currentState: MediaViewerPageControlBar.State,
+        scrollOffsetX: Double,
+        scrollAreaWidth: Double
+    ) -> PageControlBarInteractivePagingAction? {
+        let progress0To2 = scrollOffsetX / scrollAreaWidth
+        let isMovingToNextPage = progress0To2 > 1
+        let rawProgress = isMovingToNextPage ? (progress0To2 - 1) : (1 - progress0To2)
+        let progress = min(max(rawProgress, 0), 1)
+        
+        switch currentState {
+        case .collapsing, .collapsed, .expanding, .expanded:
+            // Prevent start when paging is finished and progress is reset to 0.
+            guard progress != 0 else { return nil }
+            return .start(forwards: isMovingToNextPage)
+        case .transitioningInteractively(_, let forwards):
+            if progress == 1 {
+                return .finish
+            } else if progress == 0 || forwards != isMovingToNextPage {
+                // progress is 0 or direction is changed
+                /*
+                 NOTE:
+                 Since the progress value sometimes jumps over zero,
+                 the direction change is also checked.
+                 */
+                return .cancel
+            } else {
+                return .update(progress: progress)
+            }
+        case .reloading:
+            return nil
+        }
+    }
+}
